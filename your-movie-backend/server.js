4afc951bab3e0366e821ce55ef3c7f1d458cf315
Aayushya Shrivastava <152988388+lifeline024@users.noreply.github.com>
2025-06-17 17:26:31 +0530

    Update server.js

--- your-movie-backend/server.js
+++ your-movie-backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://watchly-movie.onrender.com/',
   credentials: true
 }));
 app.use(express.json());
