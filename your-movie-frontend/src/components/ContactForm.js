3ca50b73f149ec732b3b889168223f1f8b9ff2ce
Aayushya Shrivastava <152988388+lifeline024@users.noreply.github.com>
2025-06-17 17:08:38 +0530

    Update ContactForm.js

--- your-movie-frontend/src/components/ContactForm.js
+++ your-movie-frontend/src/components/ContactForm.js
@@ -19,7 +19,7 @@ export default function Contact() {
     e.preventDefault();
     setIsSubmitting(true);
     try {
-      const res = await axios.post('http://localhost:5000/api/contact', formData);
+      const res = await axios.post('https://watchly-leod.onrender.com/api/contact', formData);
       setResponseMsg({ text: res.data.message, type: 'success' });
       setFormData({ name: '', email: '', subject: '', message: '' });
     } catch (err) {
